545c3afcb6aa5cfefe0c6020f1605a7f5068eb80
SANCHI3 <167639420+SANCHI3@users.noreply.github.com>
2026-04-12 20:16:16 +0530

    Update WebConfig.java

--- src/main/java/com/ambition/WebConfig.java
+++ src/main/java/com/ambition/WebConfig.java
@@ -1,4 +1,4 @@
-package com.ambition.ambitionbackend.config;
+package com.ambition;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.*;
