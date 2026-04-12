d8cd182afdd7946cde593e5befd4f7f731659102
SANCHI3 <167639420+SANCHI3@users.noreply.github.com>
2026-04-12 20:23:54 +0530

    Update GlobalCorsConfig.java

--- src/main/java/com/ambition/GlobalCorsConfig.java
+++ src/main/java/com/ambition/GlobalCorsConfig.java
@@ -1,4 +1,4 @@
-package com.ambition.ambitionbackend;  // 🔥 IMPORTANT (same root package)
+package com.ambition;  // 🔥 IMPORTANT (same root package)
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.*;
