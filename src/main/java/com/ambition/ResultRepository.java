64c003e4bbd0478ddd6debdb3ecf005078d3c300
SANCHI3 <167639420+SANCHI3@users.noreply.github.com>
2026-04-12 20:22:18 +0530

    Update ResultRepository.java

--- src/main/java/com/ambition/ResultRepository.java
+++ src/main/java/com/ambition/ResultRepository.java
@@ -1,4 +1,4 @@
-package com.ambition.ambitionbackend.repository;
+package com.ambition;
 
 import com.ambition.ambitionbackend.model.Result;
 import org.springframework.data.mongodb.repository.MongoRepository;
