33c849bf71b51b65c2353b616a4fb3ec87d4b670
SANCHI3 <167639420+SANCHI3@users.noreply.github.com>
2026-04-17 21:33:48 +0530

    Update TestController.java

--- src/main/java/com/ambition/ambitionbackend/controller/TestController.java
+++ src/main/java/com/ambition/ambitionbackend/controller/TestController.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@CrossOrigin
+@CrossOrigin(origins = "https://ambitionclassesozar.in")
 @RequestMapping("/api/tests")
 public class TestController {
 
