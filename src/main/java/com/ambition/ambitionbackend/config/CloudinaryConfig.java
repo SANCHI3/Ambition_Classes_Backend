aa4ffa17b21762d40917eb0cbf61639ca8fd080c
SANCHI3 <167639420+SANCHI3@users.noreply.github.com>
2026-04-21 22:11:29 +0530

    Update CloudinaryConfig.java

--- src/main/java/com/ambition/ambitionbackend/config/CloudinaryConfig.java
+++ src/main/java/com/ambition/ambitionbackend/config/CloudinaryConfig.java
@@ -8,11 +8,7 @@ import org.springframework.context.annotation.Configuration;
 public class CloudinaryConfig {
 
     @Bean
-    public Cloudinary cloudinary(){
-        return new Cloudinary(ObjectUtils.asMap(
-                "cloud_name", "dfwmbsrne",
-                "api_key", "229566582211349",
-                "api_secret", "CMn6RQKevV4Z86znS1PPhAj6DyI"
-        ));
-    }
+public Cloudinary cloudinary() {
+    return new Cloudinary(System.getenv("CLOUDINARY_URL"));
+}
 }
